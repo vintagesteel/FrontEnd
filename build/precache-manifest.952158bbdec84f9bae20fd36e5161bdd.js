7c64dcc4eea22543e8d2d934488c448e56ae0e37
vintagesteel <wayne.gaskill@gmail.com>
2020-06-17 14:18:50 +0000

    added the build back

---
+++ build/precache-manifest.952158bbdec84f9bae20fd36e5161bdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9da832d23394bf2be96928b76908ad8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5f42b10822c42a1c92c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d4793c8cbff2908291f5",
+    "url": "/static/js/2.f0c9aa7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f0c9aa7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f42b10822c42a1c92c",
+    "url": "/static/js/main.d3109310.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
