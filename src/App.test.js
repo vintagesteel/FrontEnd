c82e6387554e7b9161ff20fe3a7ba4ae0ca55bfc
vintagesteel <wayne.gaskill@gmail.com>
2020-06-17 15:30:09 +0000

    updated tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn to  react/i);
+  const linkElement = getByText(/learn to react/i);
   expect(linkElement).toBeInTheDocument();
 });
